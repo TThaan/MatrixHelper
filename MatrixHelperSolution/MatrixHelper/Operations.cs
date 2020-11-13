b91215da02ef3006269b10fc259f9d732d5796c0
TThaan <tthaan@gmx.de>
2020-11-13 16:11:20 +0100

    added PartialDerivations and empty Vector class

--- MatrixHelperSolution/MatrixHelper/Operations.cs
+++ MatrixHelperSolution/MatrixHelper/Operations.cs
@@ -153,11 +153,102 @@ namespace MatrixHelper
 
             return result;
         }
+        /// <summary>
+        /// Identic derivation for each dScalar/db_jk.
+        /// </summary>
+        public static Matrix Partial(float scalar, Matrix b, Func<float, float, float> derivation)
+        {
+            var result = new Matrix(b.n, b.m);
 
-        public static Matrix Partial(Matrix a, Matrix b)
+            for (int j = 0; j < b.m; j++)
+            {
+                for (int k = 0; k < b.n; k++)
+                {
+                    result[k, j] = derivation(scalar, b[k, j]);
+                }
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// ...
+        /// </summary>
+        public static Matrix Partial(float scalar, Matrix b, Func<float, float, float>[,] derivations)
+        {
+            var result = new Matrix(b.n, b.m);
+
+            // "Goto" next column of matrix b
+            for (int k = 0; k < b.n; k++)
+            {
+                // and iterate over this column to get it's derivation and
+                // put it into (the transposed) result.
+                for (int j = 0; j < b.m; j++)
+                {
+                    result[k, j] = derivations[j, k](scalar, b[j,k]);
+                }
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// The first F(=f1) gets derived with respect to each x(=x1, x2, etc),
+        /// one after the other, into the first row of the result.
+        /// </summary>
+        public static Matrix Partial(Matrix a, float scalar, Func<float, float, float>[,] derivation)
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Identic derivation for each dScalar/db_jk.
+        /// </summary>
+        public static Matrix Partial(Matrix a, Matrix b, Func<float, float, float> derivation)
+        {
+            Matrix result = new Matrix(a.m, b.m);
+
+            if (a.n == 1 && b.n == 1)
+            {
+                for (int j = 0; j < a.m; j++)
+                {
+                    for (int k = 0; k < b.m; k++)
+                    {
+                        result[j, k] = derivation(a[j, 1], b[k, 1]);
+                    }
+                }
+            }
+            else
+            {
+                throw new NotImplementedException("This function is only implemented for two (vertical) one-dimensional matrices so far.");
+            }
+            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Matrix Partial(Matrix a, Matrix b, Func<float, float, float>[,] derivations)
+        {
+            Matrix result = new Matrix(a.m, b.m);
+
+            if (a.n == 1 && b.n == 1)
+            {
+                for (int j = 0; j < a.m; j++)
+                {
+                    for (int k = 0; k < b.m; k++)
+                    {
+                        result[j, k] = derivations[j, k](a[j, 1], b[k, 1]);
+                    }
+                }
+            }
+            else
+            {
+                throw new NotImplementedException("This function is only implemented for two (vertical) one-dimensional matrices so far.");
+            }
+            return result;
+        }
+
+        //public static float TotalSum(Matrix a)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         //public static Func<Matrix, Matrix, Matrix, Matrix> f = F();
         //public static Func<Matrix, Matrix, Matrix, Matrix> F()
