9649e780144a62f532e1742649b32624370abc76
TThaan <tthaan@gmx.de>
2020-12-05 11:50:49 +0100

    added extension methods for more performant operations
    The extension methods return an already existing matrix instead of defining a new one. They also don't check the sizes of the matrices.

--- MatrixHelperSolution/MatrixHelper/ExtensionMethods.cs
+++ MatrixHelperSolution/MatrixHelper/ExtensionMethods.cs
@@ -42,7 +42,7 @@ namespace MatrixHelper
 
             return a;
         }
-        public static Matrix ForEach(this Matrix a, Func<float, float> func)
+        public static Matrix ForEach_New(this Matrix a, Func<float, float> func)
         {
             Matrix result = new Matrix(a.m, a.n);
 
@@ -56,7 +56,23 @@ namespace MatrixHelper
 
             return result;
         }
-        public static Matrix ForEach(this Matrix a, Matrix b, Func<float, float> func)
+        public static Matrix ForEach(this Matrix a, Func<float, float> func)
+        {
+            for (int j = 0; j < a.m; j++)
+            {
+                for (int k = 0; k < a.n; k++)
+                {
+                    a[j, k] = func(a[j, k]);
+                }
+            }
+
+            return a;
+        }
+        /// <summary>
+        /// This func takes the value of matrix b[j,k] as parameter and 
+        /// its' return value becomes the new value of matrix a[j,k].
+        /// </summary>
+        public static Matrix ForEach_New(this Matrix a, Matrix b, Func<float, float> func)
         {
             Matrix result = new Matrix(a.m, a.n);
 
@@ -70,5 +86,100 @@ namespace MatrixHelper
 
             return result;
         }
+        /// <summary>
+        /// This func takes the value of matrix b[j,k] as parameter and 
+        /// its' return value becomes the new value of matrix a[j,k].
+        /// </summary>
+        public static Matrix ForEach(this Matrix a, Matrix b, Func<float, float> func)
+        {
+            for (int j = 0; j < a.m; j++)
+            {
+                for (int k = 0; k < a.n; k++)
+                {
+                    a[j, k] = func(b[j, k]);
+                }
+            }
+
+            return a;
+        }
+
+        #region returning the same matrix and ignoring size checks
+
+        public static Matrix AddTo(this Matrix result, Matrix b)
+        {
+            for (int x = 0; x < b.n; x++)
+            {
+                for (int y = 0; y < b.m; y++)
+                {
+                    result[y, x] = result[y, x] + b[y, x];
+                }
+            }
+            return result;
+        }
+        public static Matrix SubtractFrom(this Matrix result, Matrix b)
+        {
+            for (int x = 0; x < b.n; x++)
+            {
+                for (int y = 0; y < b.m; y++)
+                {
+                    result[y, x] = result[y, x] - b[y, x];
+                }
+            }
+            return result;
+        }
+        public static Matrix MultiplicateWith(this Matrix result, float f)
+        {
+            for (int x = 0; x < result.n; x++)
+            {
+                for (int y = 0; y < result.m; y++)
+                {
+                    result[y, x] = result[y, x] * f;
+                }
+            }
+            return result;
+        }
+        public static Matrix DivideBy(this Matrix result, float f)
+        {
+            for (int x = 0; x < result.n; x++)
+            {
+                for (int y = 0; y < result.m; y++)
+                {
+                    result[y, x] = result[y, x] / f;
+                }
+            }
+            return result;
+        }
+        public static Matrix MultiplicateLikeHadamardWith(this Matrix result, Matrix b)
+        {
+            for (int x = 0; x < result.n; x++)
+            {
+                for (int y = 0; y < result.m; y++)
+                {
+                    result[y, x] = result[y, x] * b[y, x];
+                }
+            }
+            return result;
+        }
+        public static Matrix GetSacalarProduct(this Matrix result, Matrix a, Matrix b)
+        {
+            // For each row of matrix 'a'
+            for (int y = 0; y < a.m; y++)
+            {
+                // you take each column of matrix 'b', 
+                for (int x = 0; x < b.n; x++)
+                {
+                    // iterate over each value of a's columns and b's rows (a.n=b.m)
+                    for (int z = 0; z < a.n; z++)
+                    {
+                        // and compute their scalar product
+                        result[y, x] += a[y, z] * b[z, x];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
